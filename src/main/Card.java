f7a5ef2b214850e5daf15b2fb57eb7323bd4f1ca
emill-y <eishayadav@gmail.com>
2024-11-15 13:50:38 -0800

    Initial commit

---
+++ src/main/Card.java
@@ -0,0 +1,13 @@
+public class Card {
+    private String rank;
+    private String suit;
+    private int value;
+
+    public Card(String rank, String suit, int value) {
+        this.rank = rank;
+        this.suit = suit;
+        this.value = value;
+    }
+
+
+}
